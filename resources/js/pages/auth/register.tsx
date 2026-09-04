d9b4a66b83d2fc7e21eda5bc41dc97ecc2cb18e2
harrison opondo <harrisonopondo4696@gmail.com>
2026-09-04 23:43:55 +0300

    Enhance styling of Register button for improved visibility

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -92,7 +92,7 @@ export default function Register({ passwordRules }: Props) {
 
                             <Button
                                 type="submit"
-                                className="mt-2 w-full"
+                                className="mt-2 bg-[#277271] w-full"
                                 tabIndex={5}
                                 data-test="register-user-button"
                             >
