4a035d899fd7b7c6ea298d5a4b14b5c820d61ab8
harrison opondo <harrisonopondo4696@gmail.com>
2026-09-05 18:11:30 +0300

    Refactor dashboard routes to use controller methods for reading entries management

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReadingEntryController;
+use App\Http\Controllers\DashboardController;
+
 
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/reading-entries', [ReadingEntryController::class, 'store'])
+        ->name('reading-entries.store');
+
+    Route::delete('/reading-entries/{readingEntry}', [ReadingEntryController::class, 'destroy'])
+        ->name('reading-entries.destroy');
 });
 
 require __DIR__.'/settings.php';
