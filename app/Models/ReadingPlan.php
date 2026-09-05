11f7f5809dcee5724a9b0ec83db7d7d48847b461
harrison opondo <harrisonopondo4696@gmail.com>
2026-09-05 15:34:17 +0300

    Create ReadingPlan model with fillable attributes and relationships

--- app/Models/ReadingPlan.php
+++ app/Models/ReadingPlan.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ReadingPlan extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'duration_days',
+    ];
+
+    public function days(): HasMany
+    {
+        return $this->hasMany(PlanDay::class)->orderBy('day_number');
+    }
 }
