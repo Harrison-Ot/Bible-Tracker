a641aff16312af37c2616f2a4ff8a4a1457fffd9
harrison opondo <harrisonopondo4696@gmail.com>
2026-09-15 16:41:34 +0300

    Add avatar update route and remove unused Pest test file

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReadingEntryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\BibleController;
+use App\Http\Controllers\AvatarController;
 
 
 Route::inertia('/', 'welcome')->name('home');
@@ -12,6 +13,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::post('/reading-entries', [ReadingEntryController::class, 'store'])
         ->name('reading-entries.store');
+    Route::post('/avatar', [AvatarController::class, 'update'])->name('avatar.update');
 
     Route::delete('/reading-entries/{readingEntry}', [ReadingEntryController::class, 'destroy'])
         ->name('reading-entries.destroy');
